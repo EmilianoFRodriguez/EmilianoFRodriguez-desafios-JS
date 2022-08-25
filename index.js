330e942e065de817c23c19a87027cf5e5743f145
Emiliano F Rodriguez <emilianofederico.mmo@outlook.com>
2022-08-25 00:03:06 -0300

    Desafio completado

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ let nombre = prompt("Ingrese su nombre")
 alert(`Bienveni@ ${nombre}, veamos si puedes ver la siguiente pelicula`)
 
 let edad = parseInt(prompt("Ingrese su edad"))
+if(edad < 14){
+    alert("Eres muy pequeño para ver esta pelicula")
+} else if(edad < 18){
+    alert("Debes mirar la pelicula con un mayor")
+} else if(edad >= 18){
+    alert ("Puedes mirar la pelicula!")
+}
+
+alert ("Si puedes ver la pelicula, que la disfrutes!")
